6d94666e65f99b5f5f2de8ccb7a6afd15ba31eec
dragonclaw <jonathan.joseromero@gmail.com>
2016-05-24 14:08:27 -0400

    see the models dnd-user.js and the tweet.js for the response on the second problem

--- test/stronglooptuto/common/models/dnd-user.js
+++ test/stronglooptuto/common/models/dnd-user.js
@@ -1,3 +1,19 @@
 module.exports = function(DndUser) {
 
+  DndUser.unset = function(user, callback) {
+    user.instance.unsetAttribute('fullname');
+    user.instance.unsetAttribute('credentials');
+    user.instance.unsetAttribute('challenge');
+  };
+
+  DndUser.observe('before save', function updateTimestamp(ctx, next) {
+    if (ctx.isNewInstance == true) {
+      ctx.instance.createdAt = new Date();
+    }
+    next();
+  });
+
+  DndUser.afterRemote('unset', function(ctx, next) {
+    next();
+  });
 };
--- test/stronglooptuto/common/models/tweet.js
+++ test/stronglooptuto/common/models/tweet.js
@@ -1,3 +1,10 @@
 module.exports = function(Tweet) {
 
+  Tweet.observe('before save', function updateTimestamp(ctx, next) {
+    if (ctx.isNewInstance == true) {
+      ctx.instance.createdAt = new Date();
+    }
+    next();
+  });
+
 };
