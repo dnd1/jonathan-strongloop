189892d25bd72197125236e0a4d66055fff3497e
dragonclaw <jonathan.joseromero@gmail.com>
2016-05-24 14:48:08 -0400

    add remote method to convert the name into a nickname

--- test/stronglooptuto/common/models/dnd-user.js
+++ test/stronglooptuto/common/models/dnd-user.js
@@ -1,5 +1,5 @@
 module.exports = function(DndUser) {
-
+// createdAt como SOLAMENTE NUEVA INSTANCIA Y METODO REMOTO DONDE ESCONDE LOS ATRIBUTOS
   DndUser.unset = function(user, callback) {
     user.instance.unsetAttribute('fullname');
     user.instance.unsetAttribute('credentials');
@@ -16,4 +16,27 @@ module.exports = function(DndUser) {
   DndUser.afterRemote('unset', function(ctx, next) {
     next();
   });
+
+
+
+};
+
+//AGREGAMOS UN METODO REMOTO DONDE SE AGREGA EL NOMBRE COMPLETO EN MINISCULAS CON UN "-"+"ID"
+module.exports = function(nickname){
+
+        nickname.addId = function(name,name_id,cb) {
+          cb(null, function(name){
+            return(name.toLowerCase().replace(" ","-")+name_id);
+          });
+        }
+
+        nickname.remoteMethod(
+            'addId',
+            {
+              accepts:[
+                {arg: 'name', type: 'string'},
+                {arg: 'name_id', type: 'number'} ],
+
+              returns: {arg: 'nickname', type: 'string'}
+            });
 };
